0892f3812642897bf0448c2e0cd2d8bdfebc51ac
Urban Škudnik <urban.skudnik@gmail.com>
2012-05-19 22:10:14 +0200

    license, some comments

--- gpxjoin.py
+++ gpxjoin.py
@@ -3,12 +3,17 @@
 """
 gpxjoin.py
 
-Created by  on 2012-05-19.
-Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-"""
+Licensed under MIT License.
+
+Copyright (c) 2012, Urban Skudnik <urban.skudnik@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."""
 
 import sys
-import os
 import argparse
 from BeautifulSoup import BeautifulStoneSoup
 import datetime
@@ -19,23 +24,31 @@ def main():
 	parser = argparse.ArgumentParser(description="Join multiple gpx files")
 	parser.add_argument("gpx_files", metavar='GPX XML file', nargs="+", type=str, action='append')
 	args = parser.parse_args(sys.argv[1:])
-	xmls = list()
+	files = list()
+	
+	# To make sure our data files are attached in correct order; we don't trust file system (download order, ...)
 	for ffile in args.gpx_files[0]:
 		ffile = open(ffile, "r")
 		filecontent = ffile.readlines()[0]
 		xml = BeautifulStoneSoup(filecontent)
 		starttime = datetime.datetime.strptime(xml.find("metadata").find("time").string, gpx_time_format)
-		xmls += [[starttime, filecontent]]
+		files += [[starttime, filecontent]]
 	
-	ffiles = sorted(xmls, key=lambda *d: d[0]) 
+	ffiles = sorted(files, key=lambda *d: d[0]) 
 	
+	# GPX end tag is unnecessary from initial file
 	joined_gpx = ffiles[0][1].split("</gpx>")[0]
 	
+	# "Header" data (initial xml tag, gpx tag, metadata, etc.) is unnecessary
+	# in subsequent file, therefore we remove it, along with end GPX tag.
 	for date, ffile in ffiles[1:]:
 		header, content = ffile.split("</metadata>")
 		joined_gpx += content.split("</gpx>")[0]
+	
+	# Processed all files, append end GPX tag
 	joined_gpx += "</gpx>"
 	
+	# Filename is a combination of all files names
 	output_filename = " + ".join([f.split(".gpx.xml")[0] for f in args.gpx_files[0]]) + ".gpx.xml"
 	output_gpx = file(output_filename, "w")
 	output_gpx.write(joined_gpx)
